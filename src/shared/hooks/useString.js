e717279a446d7b19c85568498f8a2c976d8b2631
DevPeng1127 <pengveloper1127@gmail.com>
2025-11-17 14:59:23 +0900

    chore : 셋팅중

---
+++ src/shared/hooks/useString.js
@@ -0,0 +1,11 @@
+import { useMemo } from 'react';
+import useStore from '@/store/useStore';
+import { strings } from '@/constants/strings';
+
+export const useStrings = () => {
+    const language = useStore((state) => state.language);
+
+    return useMemo(() => strings[language] ?? strings.en, [language]);
+};
+
+export default useStrings;
